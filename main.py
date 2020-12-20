1c32b4cce2ae53623565d00e41c2a6685e0a07c1
skibidiby <skibidiby@outlook.com>
2020-12-20 19:47:58 +0200

    reformatting

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import json
 import random
 import os
-file = open('Bundesliga.json')
+
+file = open("Bundesliga.json")
 bundesliga_db = json.load(file)
 
 
@@ -13,25 +14,25 @@ class Manager:
         self.matchday = matchday
 
     def managerInfo(self):
-        print('Hello, ', str(self.name), ' you are managing', self.team)
+        print("Hello, ", str(self.name), " you are managing", self.team)
 
     def saveData(self):
         data = []
-        file = open('manager.json')
+        file = open("manager.json")
         file = json.load(file)
         # print(file)
         new_manager = {
-            'name': self.name,
-            'team': self.team,
-            'password': self.password,
-            'matchday': self.matchday
+            "name": self.name,
+            "team": self.team,
+            "password": self.password,
+            "matchday": self.matchday,
         }
         if not new_manager in file:
             data.append(new_manager)
             data.extend(file)
         else:
             data.extend(file)
-        with open('manager.json', 'w') as outfile:
+        with open("manager.json", "w") as outfile:
             json.dump(data, outfile)
 
 
@@ -42,64 +43,75 @@ class Fixture(object):
 
     def generateFixtures(self):
         if len(teams) % 2:
-            teams.append('Day off')
+            teams.append("Day off")
         n = len(teams)
         matches = []
         fixtures = []
         return_matches = []
         for fixture in range(1, n):
-            for i in range(n//2):
+            for i in range(n // 2):
                 matches.append((teams[i], teams[n - 1 - i]))
                 return_matches.append((teams[n - 1 - i], teams[i]))
             teams.insert(1, teams.pop())
-            fixtures.insert(len(fixtures)//2, matches)
+            fixtures.insert(len(fixtures) // 2, matches)
             fixtures.append(return_matches)
             matches = []
             return_matches = []
         print(matches)
-        with open('fixtures.json', 'w') as outfile:
+        with open("fixtures.json", "w") as outfile:
             json.dump(fixtures, outfile)
-        file = open('fixtures.json')
+        file = open("fixtures.json")
         fixtures = json.load(file)
         return fixtures
 
 
 lineup = []
 teams = []
-manager = Manager('vikic', 'Borussia Dortumnd', '123123', 1)
-Manager.saveData(manager)
+# manager = Manager("vikic", "Borussia Dortumnd", "123123", 1)
+# Manager.saveData(manager)
 
 
 def findTeam():
     for i in bundesliga_db:
-        if not i['CLUB'] in teams:
-            teams.append(i['CLUB'])
+        if not i["CLUB"] in teams:
+            teams.append(i["CLUB"])
     # print(teams)
     return teams
 
 
 def logIn():
-    name = input('Enter username')
-    password = input('Enter password')
-    file = open('manager.json')
+    name = input("Enter username")
+    password = input("Enter password")
+    file = open("manager.json")
     file = json.load(file)
     for i in range(len(file[0])):
-        if name in file[i]['name']:
-            if password in file[i]['password']:
-                print('logged in')
+        if name in file[i]["name"]:
+            if password in file[i]["password"]:
+                print("logged in")
                 current_manager = file[i]
                 return current_manager
             break
 
 
 def getMatchday(current_manager):
-    file = open('manager.json')
+    file = open("manager.json")
     file = json.load(file)
-    name = current_manager['name']
+    name = current_manager["name"]
     for i in range(len(file[0])):
-        if name in file[i]['name']:
-            matchday = file[i]['matchday']
+        if name in file[i]["name"]:
+            matchday = file[i]["matchday"]
             return matchday
+            break
+
+
+def getTeam(current_manager):
+    file = open("manager.json")
+    file = json.load(file)
+    team = current_manager["team"]
+    for i in range(len(file[0])):
+        if team in file[i]["team"]:
+            matchday = file[i]["matchday"]
+            return team
             break
 
 
@@ -116,12 +128,12 @@ def playersFromTeam(team):
     rating = 0
     players = []
     for i in bundesliga_db:
-        if i['CLUB'] == team:
-            number = number+1
+        if i["CLUB"] == team:
+            number = number + 1
             players.append(i)
             # print(number, ' ', i['NAME'], ' ', i['POSITION'])
-            rating = rating+int(i['RATING'])
-    rating = rating/number
+            rating = rating + int(i["RATING"])
+    rating = rating / number
     # return players
     return rating
     players.clear()
@@ -130,13 +142,18 @@ def playersFromTeam(team):
 def chooseLineup(team):
     starting = []
     while True:
-        choice = int(input("1-add player, 2-remove player, 3-display lineup"))
+        choice = int(
+            input("1-add player, 2-remove player, 3-display lineup, 4-save lineup")
+        )
         if choice == 1:
             number = int(input("Enter player's number"))
-            starting.append(lineup[number-1])
+            starting.append(lineup[number - 1])
         elif choice == 2:
             number = int(input("Enter player's number"))
             starting.pop(number)
+        elif choice == 4:
+            print(starting)
+            break
         else:
             print(starting)
 
@@ -148,29 +165,33 @@ def contains(list, filter):
     return False
 
 
-def nextGame(matchday):
+def nextGame(matchday, current_team):
     print(matchday)
-    file = open('fixtures.json')
-    table = open('table.json')
+    file = open("fixtures.json")
+    table = open("table.json")
     fixtures = json.load(file)
     table = json.load(table)
     data = []
     for i in fixtures[matchday]:
         # players = playersFromTeam(i[0])
-        rating_home = playersFromTeam(i[0])
-        rating_away = playersFromTeam(i[1])
-        print(i[0], ' vs ', i[1])
-        if rating_home > rating_away:
-            print('WINNER: ', i[0])
-            for t in table:
-                if t['team'] == i[0]:
-                    t['points'] = t['points']+3
+        if i[0] == current_team:
+            rating_home = current_team_rating
         else:
-            print('WINNER: ', i[1])
+            rating_home = playersFromTeam(i[0])
+            rating_away = playersFromTeam(i[1])
+        print(i[0], " vs ", i[1])
+        print("home", rating_home, "away", rating_away)
+        if rating_home > rating_away:
+            print("WINNER: ", i[0])
             for t in table:
-                if t['team'] == i[1]:
-                    t['points'] = t['points']+3
-        with open('table.json', 'r+') as f:
+                if t["team"] == i[0]:
+                    t["points"] = t["points"] + 3
+        else:
+            print("WINNER: ", i[1])
+            for t in table:
+                if t["team"] == i[1]:
+                    t["points"] = t["points"] + 3
+        with open("table.json", "r+") as f:
             f.seek(0)
             f.truncate()
             json.dump(table, f)
@@ -179,11 +200,17 @@ def nextGame(matchday):
 
 
 # team = chooseTeam()
-# playersFromTeam(team)
-# chooseLineup(team)
 # print(lineup)
 # teams = findTeam()
 # Fixture.generateFixtures(teams)
-current_manger = logIn()
-matchday = getMatchday(current_manger)
-nextGame(matchday)
+# current_manger = logIn()
+# matchday = getMatchday(current_manger)
+# current_team = getTeam(current_manger)
+# playersFromTeam(current_team)
+# chooseLineup(current_team)
+# nextGame(matchday, current_team)
+# print(current_team)
+team = chooseTeam()
+playersFromTeam(team)
+chooseLineup(team)
+print(lineup)
