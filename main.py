7f26b2c26b623844399805e054518e38685deb17
skibidiby <skibidiby@outlook.com>
2021-01-02 15:16:33 +0200

    fixed results

--- main.py
+++ main.py
@@ -262,17 +262,17 @@ def getFieldRating(team, current_team):
     # print("fwd:", (forwards_rating+midfielders_rating)/2, "def:",(defenders_rating+midfielders_rating)/2)
 
 
-def generateResult(outcome):
-    # population = [1, 2, 3, 4, 5, 6]
+def generateResult(outcome, winnerH):
+    print(outcome)
     weights = [1, 1, 1, 2, 2, 2, 2, 3, 3, 4, 5]
     index=random.choice(weights)
     outcome=abs(round(outcome))
-    if index>outcome:
-        home=index
-        away=index-outcome
+    if winnerH:
+        home=max(index, outcome)
+        away=max(index,outcome)-min(index,outcome)
     else:
-        home=outcome
-        away=outcome-index
+        home=max(index,outcome)-min(index,outcome)
+        away=max(index, outcome)
     print(home,':',away)
 
 
@@ -306,19 +306,19 @@ def nextGame(matchday, current_team, lineup):
 
         if outcome < -0.5:
             print("WINNER: ", team_name[0])
-            generateResult(outcome)
+            generateResult(outcome, True)
             # for t in table:
             #     if t["team"] == team_name[0]:
             #         t["points"] = t["points"] + 3
         elif outcome > 0.5:
             print("WINNER: ", team_name[1])
-            generateResult(outcome)
+            generateResult(outcome, False)
             # for t in table:
             #     if t["team"] == team_name[1]:
             #         t["points"] = t["points"] + 3
         else:
             print("DRAW")
-            generateResult(outcome)
+            generateResult(outcome, True)
         # with open("table.json", "r+") as f:
         #     f.seek(0)
         #     f.truncate()
